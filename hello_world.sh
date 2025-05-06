5e75f13a54b25e02a8df0b92d7eb0590499f82ab
BADAMASSI Ibrahim <ibrahimbadamassi39230@gmail.com>
2025-05-06 03:44:21 +0100

    Ajout des scripts Bash initiaux

--- hello_world.sh
+++ hello_world.sh
@@ -2,7 +2,7 @@
 #Écrire un script qui affiche le texte "Hello, World!" à l’écran.
 
 #je procede par la declaration d'une variable $Hello qui recoit comme valeure une chaine de charactere "Hello, World!"
-$Hello="Hello, World!"
+Hello="Hello, World!"
 
 #Affichage du texte "Hello, World!" à l’écran via la commande echo $Hello; qui retourne la valeur chaine de charactere "Hello, World!" comme sortie à l'ecran
 echo $Hello
