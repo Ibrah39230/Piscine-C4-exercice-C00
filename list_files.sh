d58f123bfd724ea7c649d6eaa11a11d1d9becb44
Ibrah39230 <158489622+Ibrah39230@users.noreply.github.com>
2025-05-07 11:59:48 +0000

    Set scripts as executable

--- list_files.sh
+++ list_files.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #Écrire un script pour afficher la liste de tous les fichiers du répertoire courant.
 # Affiche où on se trouve
-#echo "Répertoire courant : $(pwd)" 
+echo "Répertoire courant : $(pwd)" 
 # pwd montre le dossier actuel (Print Working Directory)
 # Liste  des fichiers :
 echo "les fichiers dans ce dossier :"
