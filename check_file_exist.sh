af7bed01306e46c8e4a8fed3c2b228298226948b
Ibrah39230 <158489622+Ibrah39230@users.noreply.github.com>
2025-05-09 08:20:12 +0000

    Set scripts as executable

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# check_file_exist.sh - Vérifie si un fichier existe
-
-# Affiche l'invite sans saut de ligne
-echo "Entrez le nom du fichier:"
-
-# Lit l'entrée utilisateur
+echo -n "Entrez le nom du fichier : "
 read filename
 
-# - 'echo -n' affiche le message sans saut de ligne
-# - 'read' stocke la saisie dans la variable 'filename'
-# Vérifie l'existence du fichier avec test -f
-
 if [ -f "$filename" ]; then
-    # Cas où le fichier existe
     echo "Le fichier '$filename' existe."
 else
-    # Cas où le fichier n'existe pas
     echo "Le fichier '$filename' n'existe pas."
 fi
